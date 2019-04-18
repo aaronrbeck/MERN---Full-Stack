6bc519b02651067c68b9fb1fb6164b0b11b5244a
Elvin Galindo <47126494+elvgalindo@users.noreply.github.com>
2019-04-18 14:21:02 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://geovannirosales:geovannirosales1@cluster0-tobkf.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true })
+mongoose.connect('mongodb+srv://geovannirosales:<password>@cluster0-tobkf.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
 
